47e483e870785833b9da83f6795e29fc4c8da65e
Ariel G Rogel <95644790+Aricoins@users.noreply.github.com>
2024-03-06 00:36:46 -0300

    gap

--- src/app/archivo/page.tsx
+++ src/app/archivo/page.tsx
@@ -17,26 +17,26 @@ const Registro = () => {
   return (
     <>
       <NavTop />
-      <div className="gap-[10px]">
+      <div className="gap-2">
       <div className="grid grid-cols-12 bg-gradient-to-r 
 from-gray-100 to-grey-600 via-grey-800 animate-gradient-x mt-20 mb-20">
-            <div className='col-span-6 mx-10 shadow-2xl'>
+            <div className='col-span-6 mx-2 shadow-2xl'>
           <ExpedientesOrdenanzas />
         </div>
-        <div className='col-span-6  mx-10 shadow-2xl'>
+        <div className='col-span-6  mx-2 shadow-2xl'>
           <ProyectosNoSancionados />
         </div>
-        <div className='col-span-6 mx-10 shadow-2xl'>
+        <div className='col-span-6 mx-2 shadow-2xl'>
           <ExpedientesComunicaciones />
    </div>
-   <div className='col-span-6 mx-10 shadow-2xl'>
+   <div className='col-span-6 mx-2 shadow-2xl'>
           <ExpedientesDeclaraciones />
         </div>
              
-        <div className='col-span-6  mx-10 shadow-2xl'>
+        <div className='col-span-6  mx-2 shadow-2xl'>
           <ExpedientesResoluciones />
         </div>
-        <div className='col-span-6 mx-10 shadow-2xl'>
+        <div className='col-span-6 mx-2 shadow-2xl'>
           <PCM />
         </div>
       </div>
