63c76b4b475691d5f7730c61cfaa84b349945d71
Aricoins <arielgarcia79@gmail.com>
2024-03-08 01:21:56 -0300

    orange

--- src/app/api/leerTablas/route.tsx
+++ src/app/api/leerTablas/route.tsx
@@ -1,14 +1,14 @@
-// import { NextResponse } from 'next/server';
-// import { gapi } from 'gapi';
+import { NextResponse } from 'next/server';
+import { sql } from '@vercel/postgres';
 
 
-
-// async function GET() {
-//   try {
-//     const { rows } = await gapi.client.sheet.values;
-//     return NextResponse.json({ rows });
-//   } catch (error) {
-//     console.error('Error reading table libros:', error);
-//     return NextResponse.json({ message: 'Error reading table libros' }, { status: 500 });
-//   }
-// }
+async function GET() {
+  try {
+    const { rows } = await sql`
+      SELECT * FROM libros`;
+    return NextResponse.json({ rows });
+  } catch (error) {
+    console.error('Error reading table libros:', error);
+    return NextResponse.json({ message: 'Error reading table libros' }, { status: 500 });
+  }
+}
