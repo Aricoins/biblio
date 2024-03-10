2a9b72600b1bff8e07cb29fe371d3ad30702dd10
Aricoins <arielgarcia79@gmail.com>
2024-03-10 01:56:21 -0300

    commit

--- src/app/components/Libro.tsx
+++ src/app/components/Libro.tsx
@@ -1,8 +1,9 @@
-"use client"
+import { FC } from "react";
 import Image from 'next/image';
-import defaultImage from "../api/assets/imagendefault.jpg"
 import Link from 'next/link';
-// Define las props que el componente Libro espera recibir
+import defaultImage from "../api/assets/imagendefault.jpg";
+import styles from './libro.module.css';
+
 interface LibroProps {
   libro: {
     titulo: string;
@@ -12,53 +13,40 @@ interface LibroProps {
     resenia: string;
     id: string;
   };
-
 }
 
-const Libro: React.FC<LibroProps> = ({ libro }) => {
+const Libro: FC<LibroProps> = ({ libro }) => {
 
-
-
-  return ( <Link href={`/libros/${libro.id}`} >
-    <div className="hover:w-9/12 duration-1000 hover:justify-center  max-w-l bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 my-14 mx-14">
-
-   <Image data-aos="flip-left"
-  data-aos-easing="ease-out-cubic"
-  data-aos-duration="1000"
-  className="text-center transition-all duration-1000 ease-in-out transform hover:scale-110 hover:brightness-150" 
-  src={libro.imagen ? libro.imagen : defaultImage} 
-  alt={libro.titulo} 
-  width={300} 
-  height={300} 
-/>
-
-<div className="flex flex-col items-center justify-center b">
-<div className="mb-2 text-l text-center font-bold tracking-tight text-gray-600 dark:text-white"><div className=" mb-3 font-2px text-gray-600 dark:text-gray-400 "> {libro.titulo}</div> </div>
-<h5 className="text-center text-xs">{libro.autor}</h5>
- {libro.decla? <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
- <p className='text-xs'>Declaración {libro.decla}</p>     </a> : null}
-         
- </div>
- </div>
- </Link>
+  return (
+   <>
+    <Link href={`/libros/${libro.id}`}>
+      <div className={styles.container} >
+        <div className={styles.imageContainer}>  
+          <Image
+            data-aos="flip-left"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="1000"
+            src={libro.imagen ? libro.imagen : defaultImage}
+            alt={libro.titulo}
+            width={300}
+            height={300}
+          />
+        </div>
+        <div className={styles.infoContainer}>
+          <div className={styles.title}>{libro.titulo}</div>
+          <h5 className={styles.author}>{libro.autor}</h5>
+          {libro.decla ? (
+            <Link href="#">
+              <a className={styles.declaLink}>
+                <p className='text-xs'>Declaración {libro.decla}</p>
+              </a>
+            </Link>
+          ) : null}
+        </div>
+      </div>
+    </Link>
+    </>
   );
 };
 
 export default Libro;
-
-
-
-//  
-//     <div class="p-5">
-//         <a href="#">
-//             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-//         </a>
-//         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-//         <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-//             Read more
-//              <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-//                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-//             </svg>
-//         </a>
-//     </div>
-// </div>
