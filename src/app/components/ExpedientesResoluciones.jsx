08f54b941ac18fe95636932f5a831063b785d35f
Aricoins <arielgarcia79@gmail.com>
2024-03-13 10:45:39 -0300

    titulos

--- src/app/components/ExpedientesResoluciones.jsx
+++ src/app/components/ExpedientesResoluciones.jsx
@@ -111,7 +111,7 @@ function ExpedientesResoluciones() {
         className={`${styles.h2} ${styles.h2Background}`}
         onClick={() => setIsComponentVisible((prevVisibility) => !prevVisibility)}
       >
-        Expedientes Resoluciones | 1988 - actualidad
+        Expedientes Resoluciones | 1983 - actualidad
       </h2>
 {isComponentVisible && (
       <div className={styles.block }>
