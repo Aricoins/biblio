943a4648cc258a2a51e875ccab753cffcae8cf43
Aricoins <arielgarcia79@gmail.com>
2024-06-13 13:20:21 -0300

    feat: navlogin

--- src/app/components/NavTop.tsx
+++ src/app/components/NavTop.tsx
@@ -2,35 +2,27 @@
 import Link from 'next/link';
 import logo from "../api/assets/moran.png"
 import Image from 'next/image';
-
 import styles from './NavTop.module.css';
 import clsx from 'clsx';
 import { usePathname } from 'next/navigation';
-import { UserButton, SignedIn } from '@clerk/nextjs';
+import { UserButton, SignedIn, SignedOut, SignInButton } from '@clerk/nextjs';
 import { useAuth } from '@clerk/nextjs';
-
-
 
 const NavTop = () => {
   const pathname = usePathname();
   const { isLoaded, userId, sessionId, getToken } = useAuth();
-
     return (
-   
-    <div className={styles.navTop}>
-     <Link  className={styles.navTop} href='/'>
+   <div className={styles.navTop}>
     <Image 
     src={logo}
     width={200} 
     height={300} 
     className={styles.navTopImage}
      alt="logotipo" priority />
-</Link>
     <div className={styles.navTopList}>
-    
       <div className={styles.item}>    
-          <Link href="/archivo" className={clsx(styles.link, {
-          [styles.link2]: pathname === '/archivo',
+       <Link href="/archivo" className={clsx(styles.link, {
+         [styles.link2]: pathname === '/archivo',
         
         })}>
           Archivo
@@ -51,7 +43,14 @@ const NavTop = () => {
         </Link>
       </div>
       <div className={styles.item2}> <li>
-{}   <SignedIn> <UserButton /> </SignedIn>
+
+      <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+      
       </li> 
     </div>
       </div>
