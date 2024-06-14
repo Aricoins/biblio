9f39101cc78bc3daf9a21d800536d1c01d1c9d3e
Aricoins <arielgarcia79@gmail.com>
2024-06-14 15:43:34 -0300

    feat:capa admin

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,58 +1,58 @@
-"use client"
+"use client";
+
 import Proyectos from './components/proyectos';
 import CrearLibro from '../libros/form/page';
 import Link from 'next/link';
-import { Modal } from 'antd';
-import {SignIn, useAuth} from '@clerk/nextjs';
-
+import { SignIn, useAuth } from '@clerk/nextjs';
+import { useUser } from "@clerk/clerk-react";
 
 const Page = () => {
- const { isSignedIn } = useAuth();
+  const { isSignedIn } = useAuth();
+  const { user } = useUser();
 
   return (
-       <>   
+    <>
+      {user && user.roles?.includes("admin") && isSignedIn ? (
+        <div style={{
+          textAlign: "center",
+          color: "white",
+          display: "flex",
+          flexDirection: "column",
+          width: "80%",
+          margin: "5%",
+          justifySelf: "center",
+          fontSize: "system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+        }}>
+          <h1 style={{ backgroundColor: "gray", borderRadius: "10px", fontFamily: "Arial" }}>Administrar datos</h1>
 
-       {isSignedIn ? 
-      (  
-<div style={{textAlign: "center", 
-color: "white",  
+          <div style={{
+            width: "90%",
+            margin: "1%",
+            marginLeft: "1%",
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr",
+            gap: "5%",
+            backgroundColor: "black"
+          }}>
+            <div style={{ width: "98%", color: "black" }}>
+              <p style={{ color: "white", fontFamily: "Arial" }}>Archivo</p>
+              <Proyectos />
+            </div>
 
-display: "flex",
-flexDirection: "column",
-width: "80%",
-margin: "5%",
-justifySelf: "center",
-fontSize: "system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
-
-}} >
-
-  <h1 style={{backgroundColor: "gray", borderRadius: "10px", fontFamily: "Arial"}}>Administrar datos</h1>
-
-    <div style={{width: "90%", 
-       margin: "1%", 
-      marginLeft: "1%", 
-      display: "grid",
-      gridTemplateColumns: "1fr 1fr", 
-      gap: "5%",
-      backgroundColor: "black"}} >
- 
-     
-       <div style= {{width: "98%", color: "black"}}>
-       <p style={{color: "white", fontFamily: "Arial"}}>Archivo</p> 
-        <Proyectos />
-      </div>
-      
-      <div style={{width: "98%", }}>
-      <p style= {{ color: "white",  fontFamily: "Arial", justifySelf: "right"}} > Biblioteca </p>
-           <CrearLibro /> 
-      </div>
-      
-    
-
-      </div> 
-    </div>
-      ) : <SignIn /> }
-    </>  );  
-}
+            <div style={{ width: "98%" }}>
+              <p style={{ color: "white", fontFamily: "Arial", justifySelf: "right" }}>Biblioteca</p>
+              <CrearLibro />
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <h4>Debe ser administrador para poder editar datos</h4>
+          <SignIn routing='hash'/>
+        </div>
+      )}
+    </>
+  );
+};
 
 export default Page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Text from './components/text';
 import imagen from '../../src/app/api/assets/concejo.png';
 import './globals.css';
 import { Typography } from "antd";
-import { ClerkProvider, SignInButton,  SignOutButton, SignedIn, SignedOut, UserButton, SignIn, UserProfile,  } from '@clerk/nextjs'
+import { ClerkProvider, SignInButton,  SignOutButton, SignedIn, SignedOut, UserButton, SignIn, UserProfile, SignUp,  } from '@clerk/nextjs'
 import { useAuth } from "@clerk/nextjs";
 
 
@@ -16,9 +16,9 @@ const Container = ' w-12/12 bg-gradient-to-b animated-gradient-x flex items-cent
 
 const Subcontainer = 'w-12/12 h-auto flex m-auto flex-col justify-center mx-0 md:flex-row rounded-lg opacity-80 relative'; // Añadida la posición
 
-const LeftContent = ' my-20 w-200px md:w-1/2 md:pt-0 md:pl-10 bg-white p-5 rounded-md';
+const LeftContent = ' my-20 ml-10 w-200px md:w-1/2 md:pt-0 md:pl-10 bg-white p-5 rounded-md';
 
-const RightContent = ' my-20 md:w-1/2 md:pt-0 md:pl-10 bg-white p-5 rounded-md';
+const RightContent = ' my-20 mx-10 md:w-1/2 md:pt-0 md:pl-10 bg-white p-5 rounded-md';
 
 const Boton = 'bg-gray-800 w-full flex justify-center items-center relative text-white rounded-md transition duration-500 hover:bg-gray-200 hover:border-gray-900 hover:text-gray-900';
 
@@ -40,23 +40,28 @@ export default function Libros() {
  
           <div className={Subcontainer}>
             <div className={LeftContent}>
-            <SignIn routing="hash"/>
+                            <SignedOut>
+                            <SignUp routing="hash" />
+                            
+                        </SignedOut>
+                        <SignedIn>
+                            <SignIn routing="hash" />
+                         
+                            </SignedIn>
 </div>
-            <div className={RightContent}>
-            
-                <Image
-                  className=' w-10/12 "filter drop-shadow-md rounded-lg my-10'
-                  src={logo}
-                  alt='logo'
-                  width={300}
-                  height={300}
-                  priority
+   <div className={RightContent}>
+    <Image
+      className='w-10/12 "filter drop-shadow-md rounded-lg my-10'
+      src={logo}
+      alt='logo'
+      width={300}
+      height={300}
+      priority
                 />
-           
-              <div>
-                <div >
-                  <Text setComplete={setComplete} />
-                </div>
+     <div>
+                <div>
+     <Text setComplete={setComplete} />
+               </div>
                 {showWelcome && (
                   <p
                     className={` my-4 text-base font-semibold ${
@@ -67,8 +72,7 @@ export default function Libros() {
                   </p>
                 )}
               </div>
-            
-            </div>
+              </div>
    </div>
   
           </div>
