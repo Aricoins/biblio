1394e17a7e441e80ecfcc1d2379cdfd032374331
Aricoins <arielgarcia79@gmail.com>
2024-06-15 21:25:54 -0300

    signup

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { SignIn } from "@clerk/nextjs";
+import { SignUp } from "@clerk/nextjs";
 
 export default function SignInPage() {
-  return (<div style={{display: "flex", justifyContent: "center", backgroundColor: "black"}}> <SignIn routing="hash" /></div>);
+  return (<div style={{display: "flex", justifyContent: "center", backgroundColor: "black"}}> <SignUp routing="hash" /></div>);
 }
