0bcc6be0a25148c350bcae8df3d115bc953938b8
Ariel G Rogel <95644790+Aricoins@users.noreply.github.com>
2024-07-12 12:02:55 -0300

    Update ExpedientesOrdenanzas.jsx

--- src/app/components/ExpedientesOrdenanzas.jsx
+++ src/app/components/ExpedientesOrdenanzas.jsx
@@ -126,7 +126,7 @@ function ExpedientesOrdenanzas() {
         className={styles.h2}
         onClick={() => setIsComponentVisible((prevVisibility) => !prevVisibility)}
       >
-       Ordenanzas | 2023 - 2014 
+       Ordenanzas | 2023 - 2010
       </h2>
       {isComponentVisible && (
         <div className={`${styles.block}`}>
