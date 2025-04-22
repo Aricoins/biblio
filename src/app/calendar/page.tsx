9ee5d942d77ea8a7c79b9a4aa48eb9f1b4568a94
Aricoins <arielgarcia79@gmail.com>
2025-04-22 14:26:20 -0300

    add google calendar

---
+++ src/app/calendar/page.tsx
@@ -0,0 +1,23 @@
+import NavFoot from "../components/NavFoot";
+import NavTop from "../components/NavTop"
+
+export default function CalendarPage() {
+    return (
+      <><NavTop /><main style={{ display: "flex", flexDirection: "column", alignItems: "center", margin: "5%", width: "100%", fontFamily: "Sans-Serif", color: "black", fontSize: "2vw", opacity: 0.6}}>
+        <h4>Calendario de Efemérides</h4>
+        <p style={{fontSize: "14px"}}>CONSOLIDACION NORMATIVA. ESTABLECE CALENDARIO DE CONMEMORACIONES, EFEMÉRIDES Y CELEBRACIONES MUNICIPALES.</p>
+        <p>Anexo II</p>
+        <div>
+          <iframe
+            src="https://calendar.google.com/calendar/embed?src=digestoconcejo%40gmail.com&ctz=America%2FBuenosAires"
+            style={{ border: "medium dashed green",   margin: "0", padding: "2%", borderRadius: "10px", backgroundColor: "orangered" }}
+            width="700vh"
+            height="250%"
+          
+          ></iframe>
+        </div>
+      </main>
+      <NavFoot/> </>
+    );
+  }
+  
