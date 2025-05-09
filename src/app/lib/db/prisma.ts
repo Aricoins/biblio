0add73c9ab73eab39d38499a32785971d195928f
Aricoins <arielgarcia79@gmail.com>
2025-05-09 13:27:21 -0300

    feat: add bot ai

--- src/app/lib/db/prisma.ts
+++ src/app/lib/db/prisma.ts
@@ -1,14 +1,20 @@
 import { PrismaClient, Prisma } from '@prisma/client';
 
 const globalForPrisma = global as unknown as {
-    prisma: PrismaClient | undefined;
-  };
-  const prismaOptions: Prisma.PrismaClientOptions = {
-    log: [
-      { level: 'error', emit: 'stdout' },
-      { level: 'warn', emit: 'stdout' }
-    ],
-}
+  prisma: PrismaClient | undefined;
+};
+
+const prismaOptions: Prisma.PrismaClientOptions = {
+  log: [
+    { level: 'error', emit: 'stdout' },
+    { level: 'warn', emit: 'stdout' }
+  ],
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL
+    }
+  }
+};
 
 // Crear o reutilizar la instancia de PrismaClient
 export const prisma = globalForPrisma.prisma ?? new PrismaClient(prismaOptions);
