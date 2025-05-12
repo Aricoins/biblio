7c5f8b3ab578d12ae3c4dd092da9db75c542a7d7
Aricoins <arielgarcia79@gmail.com>
2025-05-12 13:38:29 -0300

    feat(juntas): agrego buscador de estatutos de juntas vecina

--- src/app/api/verProyectos/route.tsx
+++ src/app/api/verProyectos/route.tsx
@@ -4,13 +4,6 @@ import proyectos from "./proyectos.json"
 
 export async function GET(req: NextRequest, res: NextResponse) {
   try {
-    // Consulta a la base de datos
-    // const data = await sql`SELECT resenia, titulo, autor, imagen, id, decla FROM "libros";`;
-    // const libros = data.rows;
-
-    console.log(proyectos, "libros json");
-
-    // Respuesta con los libros y encabezados para evitar caché
     return new NextResponse(JSON.stringify({ proyectos }))
   } catch (error) {
     console.error('Error al llamar los libros:', error);
