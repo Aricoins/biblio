87322bbbffd86ec49348d9a83e61e32fefffb2fb
Aricoins <arielgarcia79@gmail.com>
2025-06-25 14:53:25 -0300

    feat: new  ordenanzas image to gallery main

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -6,6 +6,7 @@ const imagenes = [
   { id: 1, src: 'https://res.cloudinary.com/dx0htqhaq/image/upload/v1745857379/tw7i2g7osiixkdmduvpa.jpg', alt: 'Image 1' },
   { id: 2, src: 'https://res.cloudinary.com/dx0htqhaq/image/upload/v1745857379/zdiwehgaawqwhnb2zxui.jpg', alt: 'Image 2' },
   { id: 3, src: 'https://res.cloudinary.com/dx0htqhaq/image/upload/v1745857378/b5pfrtkmryqlxfsiybiv.jpg', alt: 'Image 3' },
+  { id: 4, src: 'https://res.cloudinary.com/dx0htqhaq/image/upload/v1750860075/20250428_110300_scxqoc.png', alt: 'Image 4' },
 ];
 
 function Gallery() {
